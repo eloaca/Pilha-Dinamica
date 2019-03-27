774246170d90703a3a982002528dc86b099f66bd
eloaca <42654458+eloaca@users.noreply.github.com>
2019-03-27 00:58:03 -0300

    Exercicio 1
    * Exercício 1. Escreva um programa que receba duas strings x e y do usuário e determine se y é o inverso de x (isto é, se x = "ROMA", y deve equivaler a "AMOR"). Como saída, seu programa deverá exibir uma mensagem informativa.

--- src/com/pilha/Main.java
+++ src/com/pilha/Main.java
@@ -5,10 +5,15 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-
+		
 		// Criando as pilhas
 		Pilha p1 = null;
-
+		Pilha p2 = null;
+		
+		// Criando 2 pilha auxiliar
+		Pilha aux = null;
+		Pilha aux2 = null;
+		
 		// Scanner para a leitura de entrada de dados
 		Scanner leitura = new Scanner (System.in);
 		
@@ -16,11 +21,17 @@ public class Main {
 		System.out.println("Entre com a primeira palavra: ");
 		String palavra1 = leitura.next();
 		
+		System.out.println("Entre com a segunda palavra: ");
+		String palavra2 = leitura.next();
+		
 		// Try-catch
 		try {
 			// Instanciando a Pilha
 			p1 = new Pilha ();
-
+			p2 = new Pilha ();
+			aux = new Pilha ();
+			aux2 = new Pilha ();
+			
 			//Contador i
 			int i;
 			
@@ -28,17 +39,45 @@ public class Main {
 			for (i = 0; i < palavra1.length(); i++) 
 				p1.push(palavra1.charAt(i));
 			
-			// Imprimir a pilha de palavras
+			// Empilhar a palavra2 por caracter
+			for (i = 0; i < palavra2.length(); i++) 
+				p2.push(palavra2.charAt(i));
 			
-			System.out.println ("Esta é sua pilha de palavras: ");
-			while (!p1.isEmpty()){
-				System.out.println ("" +p1.peek());
-				p1.pop();
-			}
+			// Se as duas pilhas nao for do mesmo tamanho, nem entra na condição
+			int cp1 = p1.size();
+			int cp2 = p2.size();
+			
+			
+			if (cp1 == cp2) {
+				while (!p1.isEmpty()) {
+					aux.push(p1.peek());
+					p1.pop();
+				}
+				
+				int caux = aux.size();
+				
+				for (i = 0; i < caux; i++) {
+					char topo1 = (char) aux.peek();
+					char topo2 = (char) p2.peek();
+					
+					if (topo1 == topo2) {
+						p1.push(topo1);
+						aux2.push(topo2);
+						aux.pop();
+						p2.pop();
+					}
+				}
+				
+				if (aux.isEmpty() == true) {
+					System.out.println ("A palavra '"+palavra1+"' é o inverso da palavra '"+palavra2+"'");
+					System.exit(0);
+				}
+			} 
+			System.out.println("As palavras '"+palavra1+"' e '"+palavra2+ "' nao sao inversas");
 			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}			
+		}		
 	}
 }
