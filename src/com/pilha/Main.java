e42ceb52a68c844eb5e7f37b75f2fd8fdeaefd6f
Eloá Cardozo Augusto <42654458+eloaca@users.noreply.github.com>
2019-03-29 16:21:45 -0300

    1
    1

--- src/com/pilha/Main.java
+++ src/com/pilha/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
 		// Criando as pilhas
 		Pilha p1 = null;
@@ -12,33 +12,42 @@ public class Main {
 		// Scanner para a leitura de entrada de dados
 		Scanner leitura = new Scanner (System.in);
 		
-		// Entrada dos dados
-		System.out.println("Entre com a primeira palavra: ");
-		String palavra1 = leitura.next();
+		// Quantidade de visitantes
+		int v = 0;
 		
-		// Try-catch
-		try {
-			// Instanciando a Pilha
-			p1 = new Pilha ();
-
+		// Contador do laço while
+		int x = 0;
+		
+		// Instanciando a Pilha
+		p1 = new Pilha ();
+		
+		System.out.println ("Olá.\nQuantos visitantes irão participar ?");
+		v = leitura.nextInt();
+		
+		while (x != v) {
+			// Entrada dos dados
+			System.out.println("Entre com a primeira palavra: ");
+			String palavra1 = leitura.next();
+				
 			//Contador i
 			int i;
-			
-			// Empilhar a palavra1 por caracter
-			for (i = 0; i < palavra1.length(); i++) 
-				p1.push(palavra1.charAt(i));
-			
-			// Imprimir a pilha de palavras
-			
-			System.out.println ("Esta é sua pilha de palavras: ");
-			while (!p1.isEmpty()){
-				System.out.println ("" +p1.peek());
-				p1.pop();
-			}
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}			
+				
+				// Empilhar a palavra1 por caracter
+				for (i = 0; i < palavra1.length(); i++) { 
+					p1.push(palavra1.charAt(i));
+				}
+				p1.push(" "); 
+				
+			x++;
+		} // final while
+		
+		// Imprimir a pilha de palavras
+		
+		System.out.println ("Esta é sua pilha de palavras: ");
+		
+		while (!p1.isEmpty()){
+			System.out.println ("" +p1.peek());
+			p1.pop();
+		} // final while
 	}
 }
